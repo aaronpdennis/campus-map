185fb3a793fcfb8479b6cbe4aefa84ae8c56b43a
aaronpdennis <aaron.p.dennis@gmail.com>
2015-10-28 15:37:23 -0400

    adopted to mapbox gl

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# campus-map
-
-This is the GitHub repository for our prototype Penn State campus map website. You can visit the map at http://pennstategeog467.github.io/campus-map/.
+A Mapbox GL prototype campus map.
--- js/load-map.js
+++ js/load-map.js
@@ -1,18 +1,15 @@
 // This access token allows us to use Mapbox's API
-L.mapbox.accessToken = 'pk.eyJ1IjoiYWFyb25kZW5uaXMiLCJhIjoiem5LLURoYyJ9.T3tswGTI5ve8_wE-a02cMw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25kZW5uaXMiLCJhIjoiem5LLURoYyJ9.T3tswGTI5ve8_wE-a02cMw';
 
 // Set the maximum bounds so the map user doesn't pan too far from campus
-var southWest = L.latLng(40.7643, -77.9043), // south-west corner of maximum view
-    northEast = L.latLng(40.8844, -77.8064), // north-east corner of maximum view
-    bounds = L.latLngBounds(southWest, northEast); // combine the two corners to set the bounds
+var southWest = [40.7643, -77.9043], // south-west corner of maximum view
+    northEast = [40.8844, -77.8064]; // north-east corner of maximum view
 
-// Add a basic slippy map to the webpage using Leaflet.js and save it as the variable map
-var map = L.mapbox.map('map', 'jldifrisco.campus', { // Create a new map using the map tiles 'aarondennis.f6516522' in the map div
-    maxBounds: bounds, // set the maximum pan boundaries to bounds
-    maxZoom: 19, // set how far into the map you can zoom to level 19
-    minZoom: 15  // set how far out from the map you can zoom to level 15
-  }).setView([40.7967, -77.8619], 18); // set the map view to those coordinates and zoom level 18 (Old Main)
-
-L.control.locate().addTo(map); // Adds the geolocate button to the map
-
-var hash = L.hash(map); // Makes the webpage URL change so that the coordinates are included
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/aarondennis/cigb5ah9x000kn8kq46wym4ge',
+  hash: false,
+  center: [-77.86, 40.8],
+  bearing: -45,
+  zoom: 14.5
+});
