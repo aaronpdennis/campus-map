06e620394eea20901fa584e0dd8f750e2ad1d38a
aaronpdennis <aaron.p.dennis@gmail.com>
2016-01-07 16:55:08 -0500

    new style

--- js/add-transit.js
+++
@@ -1,7 +0,0 @@
-// CATA publishes live bus locations in JSON format (?) to the URL below
-// http://realtime.catabus.com/InfoPoint/rest/vehicles/getallvehicles
-
-//POPULATE MYAVAIL PROPERTIES FROM SERVER SIDE WHEN NECESSARY
-
-var geojsonLayer = L.geoJson.ajax("http://realtime.catabus.com/InfoPoint/rest/vehicles/getallvehicles",{dataType:"jsonp"});
-console.log(geojsonLayer);
